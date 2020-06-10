938de26b925878c338487904ec5d63103efa868f
THECODE-MASTER <63726176+THECODE-MASTER@users.noreply.github.com>
2020-06-10 18:58:01 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var chain1,circle1,platform1;
+
+function setup(){
+     canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+    circle1=new circle(200,200,100);
+     platform1=new PLatform(600,10,1200,50);
+    //log6 = new Log(230,180,80, PI/2);
+    chain1 = new SlingShot(circle1,platform1);;
+}
+
+function draw(){
+  Engine.update(engine);
+  background("black")
+  platform1.display();
+  circle1.display(600,200);
+ chain.display();
+}
+
+
+
+
+
+
+
+
